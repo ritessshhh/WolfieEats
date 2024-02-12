26580c28c0aa6fac27fa63a1c41bc2c9d78f3b80
Atri-lab <atrivyas7@gmail.com>
2024-02-12 10:38:57 -0500

    Initial Commit

---
+++ WolfieEats/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WolfieEats
+//
+//  Created by Atri Vyas on 2/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WolfieEats/WolfieEatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WolfieEatsApp.swift
+//  WolfieEats
+//
+//  Created by Atri Vyas on 2/12/24.
+//
+
+import SwiftUI
+
+@main
+struct WolfieEatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
